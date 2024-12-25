7b259d48452f27de771805356338dbbe289cd2bc
9taxo1 <165591399+9taxo1@users.noreply.github.com>
2024-12-25 20:43:34 +0300

    Update adminfinder.py

--- adminfinder.py
+++ adminfinder.py
@@ -18,16 +18,15 @@ print ('''\033[1;34m______   ______ _______ _______ _______ _     _ _______  ___
 |_____] |_____/ |______ |_____| |       |_____| |______ |_____/
 |_____] |    \_ |______ |     | |_____  |     | |______ |    \_
 
-                          \033[37mMade with \033[91m<3\033[37m By D3V\033[1;m''')
+                          \033[37mMade with \033[91m<3\033[37m By Taxo[1;m''')
 
-print ('''\n  I am not responsible for your shit and if you get some error while
- running Breacher, there are good chances that target isn't responding.\n''')
+print ('''\n  Yaptığınız şeylerden ben sorumlu değilim ve eğer bir hata alırsanız bana danışınız hedefin yanıt vermeme ihtimali yüksek.\n''')
 print ('\033[1;31m--------------------------------------------------------------------------\033[1;m\n')
 
 try:
     target = target.replace('https://', '') #Removes https://
 except:
-    print ('\033[1;31m[-]\033[1;m -u argument is not supplied. Enter python breacher -h for help')
+    print ('\033[1;31m[-]\033[1;m -u argüman sağlanamadı. -h komutunu kullanıp komut sayfasından yardım alabilirsiniz')
     quit()
 
 target = target.replace('http://', '') #and http:// from the url
@@ -38,12 +37,12 @@ if args.prefix != None:
 try:
     r = requests.get(target + '/robots.txt') #Requests to example.com/robots.txt
     if '<html>' in r.text: #if there's an html error page then its not robots.txt
-        print ('  \033[1;31m[-]\033[1;m Robots.txt not found\n')
+        print ('  \033[1;31m[-]\033[1;m Robots.txt bulunamadı\n')
     else: #else we got robots.txt
-        print ('  \033[1;32m[+]\033[0m Robots.txt found. Check for any interesting entry\n')
+        print ('  \033[1;32m[+]\033[0m Robots.txt bulundu. İlginç bir giriş olup olmadığını kontrol edin\n')
         print (r.text)
 except: #if this request fails, we are getting robots.txt
-    print ('  \033[1;31m[-]\033[1;m Robots.txt not found\n')
+    print ('  \033[1;31m[-]\033[1;m Robots.txt bulunamadı\n')
 print ('\033[1;31m--------------------------------------------------------------------------\033[1;m\n')
 
 def scan(links):
@@ -52,11 +51,11 @@ def scan(links):
         r = requests.get(link) #Requests to the combined url
         http = r.status_code #Fetches the http response code
         if http == 200: #if its 200 the url points to valid resource i.e. admin panel
-            print ('  \033[1;32m[+]\033[0m Admin panel found: %s'% link)
+            print ('  \033[1;32m[+]\033[0m Admin panel bulundu: %s'% link)
         elif http == 404: #404 means not found
             print ('  \033[1;31m[-]\033[1;m %s'% link)
         elif http == 302: #302 means redirection
-            print ('  \033[1;32m[+]\033[0m Potential EAR vulnerability found : ' + link)
+            print ('  \033[1;32m[+]\033[0m Potansiyel EAR Açığı Bulundu  : ' + link)
         else:
             print ('  \033[1;31m[-]\033[1;m %s'% link)
 paths = [] #list of paths
@@ -84,7 +83,7 @@ def get_paths(type):
                 except:
                     paths.append(path)
     except IOError:
-        print ('\033[1;31m[-]\033[1;m Wordlist not found!')
+        print ('\033[1;31m[-]\033[1;m Wordlist Bulunamadı Wordlist Adını Kontrol Ediniz "paths.txt" Olmasına Dikkat!')
         quit()
 
 if args.fast == True: #if the user has supplied --fast argument
